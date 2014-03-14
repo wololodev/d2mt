dffa0c1e913506543895d989c1a2809918003c10
Matthew Peveler <matt.peveler@gmail.com>
2014-03-14 15:55:14 -0400

    Fix for #7 and #9 (merges #8)

--- api/jd/matches/v130/index.php
+++ api/jd/matches/v130/index.php
@@ -1,7 +1,10 @@
 <?php
+	//error_reporting(0);
 	require_once('php/simple_html_dom.php');
 	date_default_timezone_set("America/Montreal");
-	$html = file_get_contents('http://www.joindota.com/en/matches');
+	$url = "http://www.joindota.com/en/matches/&c1=&c2=&c3=&archiv_page=";
+	$page = 1;
+	$html = file_get_contents($url.$page);
 	$matchList = new simple_html_dom();
 	$matchList->load($html);
 	$titleList = new simple_html_dom();
@@ -21,7 +24,7 @@
 		$date = trim($date);
 		//check to see if there's a ':' in the string (denonating a finished game)
 		if (!strpos($date, ':')) {
-			$id = substr($id, -5);
+			$id = str_replace('score_','',$id);
 			$linkID = "http://www.joindota.com/en/matches/{$id}";
 			$titleExt = file_get_contents($linkID);
 			$titleList->load($titleExt);
@@ -36,44 +39,53 @@
 			}
 		}
 	}
-	foreach($matchList->find('.pad .item') as $aGame) {
-		if ($i < 10) {
-			$winner = str_replace(' ', '', $aGame->find('.sub', 2)->plaintext);
-			if (strlen($winner) != 3)
-				continue;
-			$img1 = $aGame->find('img', 0)->src;
-			$img1tit = $aGame->find('img', 0)->title;
-			$team1 = $aGame->find('.sub', 1)->plaintext;
+	while ($i < 10) {
+		foreach($matchList->find('.pad .item') as $aGame) {
+			if ($i < 10) {
+				
+				$winner = str_replace(' ', '', $aGame->find('.sub', 2)->plaintext);
+				if (strlen($winner) != 3)
+					continue;
+				$img1 = $aGame->find('img', 0)->src;
+				$img1tit = $aGame->find('img', 0)->title;
+				$team1 = $aGame->find('.sub', 1)->plaintext;
 
-			$img2  = $aGame->find('img', 1)->src;
-			$img2tit  = $aGame->find('img', 1)->title;
-			$team2 = $aGame->find('.sub', 3)->plaintext;
+				$img2  = $aGame->find('img', 1)->src;
+				$img2tit  = $aGame->find('img', 1)->title;
+				$team2 = $aGame->find('.sub', 3)->plaintext;
 
-			$id = substr($aGame->href, 35);
-			$winnerCheck = explode(':', $winner);
-			if ((int)$winnerCheck[0] > (int)$winnerCheck[1]) {
-				$team1 = "<b>{$team1}</b>";
-				$vs = '>';
-			} else if ((int)$winnerCheck[0] < (int)$winnerCheck[1]){
-				$team2 = "<b>{$team2}</b>";
-				$vs = '<';
+				$id = substr($aGame->href, 35);
+				$winnerCheck = explode(':', $winner);
+				if ((int)$winnerCheck[0] > (int)$winnerCheck[1]) {
+					$team1 = "<b>{$team1}</b>";
+					$vs = '>';
+				} else if ((int)$winnerCheck[0] < (int)$winnerCheck[1]){
+					$team2 = "<b>{$team2}</b>";
+					$vs = '<';
+				} else {
+					$vs = 'x';
+				}
+				
+				$linkID = $aGame->find('a', 0)->href;
+				$titleExt = file_get_contents($linkID);
+				$titleList->load($titleExt);
+				$titleStr = $titleList->find('.match_head .left', 0)->plaintext;
+				$date = $titleList->find('.match_head .right', 0)->plaintext;
+				
+				$timeStamp = strtotime($date);
+				//$whenAdv = date('j/m/Y G:i T', $timeStamp);
+				$finishedList["eventDone"][] =  "<tr class='d2mtrow eventDone' href='{$linkID}' title='{$titleStr}' rel='tooltip' id='{$id}'><td class='jd_date push-tt series' alt='{$timeStamp}'>{$winner}</td><td><img title='{$img1tit}' src='{$img1}' width='14px' height='9px'> <span>{$team1}</span></td><td class='winResult' data-winner='{$vs}'>{$vs}</td><td><img title='{$img2tit}' src='{$img2}' width='14px' height='9px'> <span>{$team2}</span></td></tr>";
+				/**/
+				print $i;
+				$i++;
 			} else {
-				$vs = 'x';
+				break 2;
 			}
-			
-			$linkID = $aGame->find('a', 0)->href;
-			$titleExt = file_get_contents($linkID);
-			$titleList->load($titleExt);
-			$titleStr = $titleList->find('.match_head .left', 0)->plaintext;
-			$date = $titleList->find('.match_head .right', 0)->plaintext;
-			
-			$timeStamp = strtotime($date);
-			//$whenAdv = date('j/m/Y G:i T', $timeStamp);
-			$finishedList["eventDone"][] =  "<tr class='d2mtrow eventDone' href='{$linkID}' title='{$titleStr}' rel='tooltip' id='{$id}'><td class='jd_date push-tt series' alt='{$timeStamp}'>{$winner}</td><td><img title='{$img1tit}' src='{$img1}' width='14px' height='9px'> <span>{$team1}</span></td><td class='winResult' data-winner='{$vs}'>{$vs}</td><td><img title='{$img2tit}' src='{$img2}' width='14px' height='9px'> <span>{$team2}</span></td></tr>";
-			$i++;
-		} else {
-			break;
 		}
+		$page++;
+		$html = file_get_contents($url.$page);
+		print $url.$page."<br />";
+		$matchList->load($html);
 	}
 
 	$output["eventSoon"] = array_reverse($output["eventSoon"]); 
