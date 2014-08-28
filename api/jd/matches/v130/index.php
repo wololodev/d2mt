19b06dc00bddab488e2114c5d694a7046d6f6f94
Matthew Peveler <matt.peveler@gmail.com>
2014-08-28 01:19:04 -0400

    Limit upcoming JD games to 15

--- api/jd/matches/v130/index.php
+++ api/jd/matches/v130/index.php
@@ -8,33 +8,42 @@
 	$output = array();
 	$finishedList = array();
 	$i = 0;
-	foreach($matchList->find('div[id=matchticker] .item') as $aGame) {
-		$img1 = $aGame->find('img', 0)->src;
-		$img1tit = $aGame->find('img', 0)->title;
-		$team1 = $aGame->find('.sub', 0)->plaintext;
-		$img2  = $aGame->find('img', 1)->src;
-		$img2tit  = $aGame->find('img', 1)->title;
-		$team2 = $aGame->find('.sub', 2)->plaintext;
-		$date = $aGame->find('.sub', 3)->plaintext;
-		$id = $aGame->find('div', 3)->id;
-		
-		$date = trim($date);
-		//check to see if there's a ':' in the string (denonating a finished game)
-		if (!strpos($date, ':')) {
-			$id = str_replace('score_','',$id);
-			$linkID = "http://www.joindota.com/en/matches/{$id}";
-			$titleList = file_get_html($linkID);
-			$titleStr = $titleList->find('.match_head .left', 0)->plaintext;
-			$timeStampOrig = $titleList->find('.match_head .right', 0)->plaintext;
-			$timeStamp = strtotime($timeStampOrig);
-			if ($date !== 'LIVE' && $date !== 'now') {
-				$output["eventSoon"][] =  "<tr class='d2mtrow eventSoon' href='{$linkID}' title='{$titleStr}' rel='tooltip' id='{$id}'><td alt='{$timeStamp}' class='push-tt jd_date'>{$date}</td><td><img src='{$img1}' title='{$img1tit}' width='14px' height='9px'> {$team1}</td><td>v</td><td><img title='{$img2tit}' src='{$img2}' width='14px' height='9px'> {$team2}</td></tr>";
-			} else {
-				$date = "Live";
-				$output["eventLive"][] = "<tr href='{$linkID}' class='d2mtrow eventLive' title='{$titleStr}' rel='tooltip' id='{$id}'><td alt='{$timeStamp}' class='push-tt jd_date'><b>{$date}</b></td><td><img title='{$img1tit}' src='{$img1}' width='14px' height='9px'> {$team1}</td><td>v</td><td><img title='{$img2tit}' src='{$img2}' width='14px' height='9px'> {$team2}</td></tr>";
+	foreach(array_reverse($matchList->find('div[id=matchticker] .item')) as $aGame) {
+		if ($i < 16) {
+			$img1 = $aGame->find('img', 0)->src;
+			$img1tit = $aGame->find('img', 0)->title;
+			$team1 = $aGame->find('.sub', 0)->plaintext;
+			$img2  = $aGame->find('img', 1)->src;
+			$img2tit  = $aGame->find('img', 1)->title;
+			$team2 = $aGame->find('.sub', 2)->plaintext;
+			$date = $aGame->find('.sub', 3)->plaintext;
+			$id = $aGame->find('div', 3)->id;
+			
+			$date = trim($date);
+			//check to see if there's a ':' in the string (denonating a finished game)
+			if (!strpos($date, ':')) {
+				$id = str_replace('score_','',$id);
+				$linkID = "http://www.joindota.com/en/matches/{$id}";
+				$titleList = file_get_html($linkID);
+				$titleStr = $titleList->find('.match_head .left', 0)->plaintext;
+				$timeStampOrig = $titleList->find('.match_head .right', 0)->plaintext;
+				$timeStamp = strtotime($timeStampOrig);
+				if ($date !== 'LIVE' && $date !== 'now') {
+					$output["eventSoon"][] =  "<tr class='d2mtrow eventSoon' href='{$linkID}' title='{$titleStr}' rel='tooltip' id='{$id}'><td alt='{$timeStamp}' class='push-tt jd_date'>{$date}</td><td><img src='{$img1}' title='{$img1tit}' width='14px' height='9px'> {$team1}</td><td>v</td><td><img title='{$img2tit}' src='{$img2}' width='14px' height='9px'> {$team2}</td></tr>";
+				} else {
+					$date = "Live";
+					$output["eventLive"][] = "<tr href='{$linkID}' class='d2mtrow eventLive' title='{$titleStr}' rel='tooltip' id='{$id}'><td alt='{$timeStamp}' class='push-tt jd_date'><b>{$date}</b></td><td><img title='{$img1tit}' src='{$img1}' width='14px' height='9px'> {$team1}</td><td>v</td><td><img title='{$img2tit}' src='{$img2}' width='14px' height='9px'> {$team2}</td></tr>";
+				}
 			}
+			$i++;
+		}
+		else {
+			break 1;
 		}
 	}
+	$output["eventSoon"] = array_reverse($output["eventSoon"]);
+
+	$i = 0;
 	while ($i < 16) {
 		foreach($matchList->find('.pad .item') as $aGame) {
 			if ($i < 16) {
