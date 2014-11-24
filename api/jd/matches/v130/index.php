9b56d4ee62e3d842be17dc510d2c76148b2049dd
Matthew Peveler <matt.peveler@gmail.com>
2014-11-24 00:46:04 -0500

    Update scraper for new jD format

--- api/jd/matches/v130/index.php
+++ api/jd/matches/v130/index.php
@@ -1,100 +1,96 @@
 <?php
-	// error_reporting(E_ALL | E_PARSE);
+	error_reporting(0);
 	require_once('php/simple_html_dom.php');
 	date_default_timezone_set("America/Montreal");
-	$url = "http://www.joindota.com/en/matches/&c1=&c2=&c3=&archiv_page=";
-	$page = 1;
-	$matchList = file_get_html($url.$page);
+	$url = "http://www.joindota.com/en/start";
+	$matchList = file_get_html($url);
 	$output = array();
 	$finishedList = array();
+	
+	$output = array("eventSoon" => array(), "eventLive" => array(), "eventDone" => array());
+
 	$i = 0;
-
-	$output = array("eventSoon"=>array(),"eventLive"=>array());
-
-	foreach(array_reverse($matchList->find('div[id=matchticker] .item')) as $aGame) {
+	foreach($matchList->find('div[id=in_matches_list] ul[class=widget-matches-list-live] li') as $aGame) {
 		if ($i < 16) {
-			$img1 = $aGame->find('img', 0)->src;
-			$img1tit = $aGame->find('img', 0)->title;
-			$team1 = $aGame->find('.sub', 0)->plaintext;
-			$img2  = $aGame->find('img', 1)->src;
-			$img2tit  = $aGame->find('img', 1)->title;
-			$team2 = $aGame->find('.sub', 2)->plaintext;
-			$date = $aGame->find('.sub', 3)->plaintext;
-			$id = $aGame->find('div', 3)->id;
+			$aGameTeams = $aGame->children(0);
 
-			$date = trim($date);
-			//check to see if there's a ':' in the string (denonating a finished game)
+			$team1 = $aGameTeams->find('.widget-matches-opp1', 0)->plaintext;
+			$team1Flag = $aGameTeams->find('img', 0);
+			$team2 = $aGameTeams->find('.widget-matches-opp2', 0)->plaintext;
+			$team2Flag = $aGameTeams->find('img', 1);
+			$linkID = $aGame->find('a', 0)->href;
+			preg_match("/\/matches\/([0-9]+)\-/",$linkID,$matches);
+			$id = $matches[1];
 
-			if (!strpos($date, ':')) {
-				$id = str_replace('score_','',$id);
-				$linkID = $aGame->attr['href'];
-				$titleList = file_get_html($linkID);
-				$titleStr = $titleList->find('.match_head .left', 0)->plaintext;
-				$timeStampOrig = $titleList->find('.match_head .right', 0)->plaintext;
-				$timeStamp = strtotime($timeStampOrig);
-				if ($date !== 'LIVE' && $date !== 'now') {
-					$output["eventSoon"][] =  "<tr class='d2mtrow eventSoon' href='{$linkID}' title='{$titleStr}' rel='tooltip' id='{$id}'><td alt='{$timeStamp}' class='push-tt jd_date'>{$date}</td><td><img src='{$img1}' title='{$img1tit}' width='14px' height='9px'> {$team1}</td><td>v</td><td><img title='{$img2tit}' src='{$img2}' width='14px' height='9px'> {$team2}</td></tr>";
-				} else {
-					$date = "Live";
-					$output["eventLive"][] = "<tr href='{$linkID}' class='d2mtrow eventLive' title='{$titleStr}' rel='tooltip' id='{$id}'><td alt='{$timeStamp}' class='push-tt jd_date'><b>{$date}</b></td><td><img title='{$img1tit}' src='{$img1}' width='14px' height='9px'> {$team1}</td><td>v</td><td><img title='{$img2tit}' src='{$img2}' width='14px' height='9px'> {$team2}</td></tr>";
-				}
+			$aGameTime = $aGame->children(1)->title;
+
+			$gamePage = file_get_html($linkID);
+			$titleStr = $gamePage->find('.match_head .left', 0)->plaintext;
+
+			if (strpos($aGameTime,"(upcoming)") === false) {
+				$timeStamp = strtotime(str_replace(" (live)", "", $aGameTime));
+				$date = "Live";
+				$output["eventLive"][] = "<tr href='{$linkID}' class='d2mtrow eventLive' title='{$titleStr}' rel='tooltip' id='{$id}'><td alt='{$timeStamp}' class='push-tt jd_date'><b>{$date}</b></td><td><img title='{$team1Flag->alt}' src='{$team1Flag->src}' width='14px' height='9px'> {$team1}</td><td>v</td><td><img title='{$team2Flag->title}' src='{$team2Flag->src}' width='14px' height='9px'> {$team2}</td></tr>";
 			}
+			else {
+				$timeStamp = strtotime(str_replace(" (upcoming)", "", $aGameTime));
+				$date = $aGame->children(1)->find('.widget-matches-score-time', 0)->plaintext;
+				$output["eventSoon"][] =  "<tr class='d2mtrow eventSoon' href='{$linkID}' title='{$titleStr}' rel='tooltip' id='{$id}'><td alt='{$timeStamp}' class='push-tt jd_date'>{$date}</td><td><img src='{$team1Flag->src}' title='{$team1Flag->alt}' width='14px' height='9px'> {$team1}</td><td>v</td><td><img title='{$team2Flag->title}' src='{$team2Flag->src}' width='14px' height='9px'> {$team2}</td></tr>";
+			}
+
 			$i++;
 		}
 		else {
 			break 1;
 		}
 	}
-	$output["eventSoon"] = array_reverse($output["eventSoon"]);
+	//$output["eventSoon"] = array_reverse($output["eventSoon"]);
 
+	
 	$i = 0;
-	while ($i < 16 && $page < 5) {
-		foreach($matchList->find('.pad .item') as $aGame) {
-			if ($i < 16) {
-				
-				$winner = trim(str_replace("(def)","",str_replace(' ', '', $aGame->find('.sub', 2)->plaintext)));
-				if (strlen($winner) != 3 || $winner == "tba")
-					continue;
-				$img1 = $aGame->find('img', 0)->src;
-				$img1tit = $aGame->find('img', 0)->title;
-				$team1 = $aGame->find('.sub', 1)->plaintext;
+	$doneMatches = $matchList->find('div[id=in_matches_list] ul[class=widget-matches-list]',1);
+	foreach($doneMatches->find('li') as $aGame) {
+		if ($i < 16) {
+			$aGameTeams = $aGame->children(0);
 
-				$img2  = $aGame->find('img', 1)->src;
-				$img2tit  = $aGame->find('img', 1)->title;
-				$team2 = $aGame->find('.sub', 3)->plaintext;
+			$team1 = $aGameTeams->find('.widget-matches-opp1', 0)->plaintext;
+			$team1Flag = $aGameTeams->find('img', 0);
+			$team2 = $aGameTeams->find('.widget-matches-opp2', 0)->plaintext;
+			$team2Flag = $aGameTeams->find('img', 1);
+			$linkID = $aGame->find('a', 0)->href;
+			preg_match("/\/matches\/([0-9]+)\-/",$linkID,$matches);
+			$id = $matches[1];
 
-				$id = substr($aGame->href, 35);
-				$winnerCheck = explode(':', $winner);
-				if ((int)$winnerCheck[0] > (int)$winnerCheck[1]) {
-					$team1 = "<b>{$team1}</b>";
-					$vs = '>';
-				} else if ((int)$winnerCheck[0] < (int)$winnerCheck[1]){
-					$team2 = "<b>{$team2}</b>";
-					$vs = '<';
-				} else {
-					$vs = 'x';
-				}
-				
-				$linkID = $aGame->find('a', 0)->href;
-				$titleList = file_get_html($linkID);
-				$titleStr = $titleList->find('.match_head .left', 0)->plaintext;
-				$date = $titleList->find('.match_head .right', 0)->plaintext;
-				$timeStamp = strtotime($date);
-				if ($winner == "tba") 
-					continue;
-				//$whenAdv = date('j/m/Y G:i T', $timeStamp);
-				$finishedList["eventDone"][] =  "<tr class='d2mtrow eventDone' href='{$linkID}' title='{$titleStr}' rel='tooltip' id='{$id}'><td class='jd_date push-tt series' alt='{$timeStamp}'>{$winner}</td><td><img title='{$img1tit}' src='{$img1}' width='14px' height='9px'> <span>{$team1}</span></td><td class='winResult' data-winner='{$vs}'>{$vs}</td><td><img title='{$img2tit}' src='{$img2}' width='14px' height='9px'> <span>{$team2}</span></td></tr>";
-				/**/
-				$i++;
-			} else {
-				break 2;
+			$timeStamp = strtotime(str_replace(" (finished)", "", $aGame->children(1)->title));
+
+			$gamePage = file_get_html($linkID);
+			$titleStr = $gamePage->find('.match_head .left', 0)->plaintext;
+
+			$winner = $aGame->find('.widget-matches-score', 0)->plaintext;
+			$winnerCheck = explode(":", $winner);
+			$winnerCheck[0] = intval($winnerCheck[0]);
+			$winnerCheck[1] = intval($winnerCheck[1]);
+			if ($winnerCheck[0] > $winnerCheck[1]) {
+				$team1 = "<b>{$team1}</b>";
+				$vs = ">";
 			}
-		}
-		$page++;
-		$matchList = file_get_html($url.$page);
-	}
+			else if ($winnerCheck[1] > $winnerCheck[0]) {
+				$team2 = "<b>{$team2}</b>";
+				$vs = "<";
+			}
+			else {
+				$vs = "x";
+			}
 
-	$output["eventSoon"] = array_reverse($output["eventSoon"]); 
+			$finishedList["eventDone"][] =  "<tr class='d2mtrow eventDone' href='{$linkID}' title='{$titleStr}' rel='tooltip' id='{$id}'><td class='jd_date push-tt series' alt='{$timeStamp}'>{$winner}</td><td><img title='{$team1Flag->alt}' src='{$team1Flag->src}' width='14px' height='9px'> <span>{$team1}</span></td><td class='winResult' data-winner='{$vs}'>{$vs}</td><td><img title='{$team2Flag->alt}' src='{$team2Flag->src}' width='14px' height='9px'> <span>{$team2}</span></td>
+			</tr>";
+
+			$i++;
+		}
+		else {
+			break 1;
+		}
+	}
 
 	$output = array_merge($output, $finishedList);
 	$str = trim(json_encode($output));
