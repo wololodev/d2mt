568ca5757f958ff15527a12c028a716bf9df3c22
gkrambs <gregorkrambs@gmail.com>
2014-12-17 11:40:28 +0100

    updated consistency in style

--- api/dd2/matches/v100/index.php
+++ api/dd2/matches/v100/index.php
@@ -16,20 +16,22 @@
 
 		$bestof = $match['series_type'];
 
-        $eventName = $match['league']['name']." [BO{$bestof}]";
+		$eventName = $match['league']['name']." [BO{$bestof}]";
 
 		$timeStamp = $match['starttime_unix'];
 
 		if ($match['status'] == 1) {
 			$date = "Live";
 			$gameArray["eventLive"][] = "<tr class='d2mtrow eventLive' href='{$linkID}' title='{$eventName}' rel='tooltip'><td alt='{$timeStamp}' class='push-tt gg_date'><b>{$date}</b></td><td><img src='{$img1}' width='14px' height='9px'> {$team1}</td><td>v</td><td><img src='{$img2}' width='14px' height='9px'> {$team2}</td></tr>";
-	    }
-	    if ($match['status'] == 0) {
-	    	$date = nice_time($match['timediff']);
-       		$gameArray["eventSoon"][] =  "<tr class='d2mtrow eventSoon' href='{$linkID}' title='{$eventName}' rel='tooltip'><td alt='{$timeStamp}' class='push-tt gg_date'>{$date}</td><td><img src='{$img1}' width='14px' height='9px'> {$team1}</td><td>v</td><td><img src='{$img2}' width='14px' height='9px'> {$team2}</td></tr>";
-        }
-        if ($i == 13) break;
-        $i++;
+		}
+		
+		if ($match['status'] == 0) {
+			$date = nice_time($match['timediff']);
+			$gameArray["eventSoon"][] =  "<tr class='d2mtrow eventSoon' href='{$linkID}' title='{$eventName}' rel='tooltip'><td alt='{$timeStamp}' class='push-tt gg_date'>{$date}</td><td><img src='{$img1}' width='14px' height='9px'> {$team1}</td><td>v</td><td><img src='{$img2}' width='14px' height='9px'> {$team2}</td></tr>";
+		}
+
+		if ($i == 13) break;
+		$i++;
 	}
 	
 	$gameArray["eventDone"][] = "<tr class='d2mtrow eventDone' href='#' title='title' rel='tooltip'><td alt='timestamp' class='push-tt gg_date series'>Will be available soon(tm).</td></tr>";
@@ -45,11 +47,8 @@
 
 	function nice_time($t) {
 		$h = floor($t/3600);
-		if ($h < 10) { $h = '0'.$h; }
 		$m = floor($t/60)%60;
-		if ($m < 10) { $m = '0'.$m; }
 		$s = floor($t%60);
-		if ($s < 10) { $s = '0'.$s; }
 
 		if ($t > 3600) {
 			return $h.'h '.$m.'m';
